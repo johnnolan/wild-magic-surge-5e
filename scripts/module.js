23ef27f45aed8558721754f652a8e1e6eaede55a
John Nolan <me@johnnolan.dev>
2021-05-08 11:22:48 +0100

    Clean up of module.js and files

--- scripts/module.js
+++ scripts/module.js
@@ -1,45 +1,26 @@
-const MODULE_NAME = "Wild Magic Surge 5e"
-console.log(
-    MODULE_NAME
-  );
+const MODULE_NAME = "Wild Magic Surge 5e";
 
 Hooks.on("init", function () {
-  console.log(
-    `Loading ${MODULE_NAME}`
-  );
+  console.log(`Loading ${MODULE_NAME}`);
 });
 
 Hooks.on("ready", function () {
-  console.log(
-    `Successfully loaded ${MODULE_NAME}`
-  );
+  console.log(`Successfully loaded ${MODULE_NAME}`);
 });
 
 Hooks.on("preRollItemBetterRolls", function (arg1, arg2, arg3) {
-  console.log(
-    `${MODULE_NAME} Check`, arg1
-  );
-
   try {
-    //const arg = arguments[0].args[3][0];
-    //const data = arg1[0].args[3][0];
     const data = arg1;
-    console.log(`Loading ${MODULE_NAME}: ${game.user}`)
-  
-    const hasWildMagicFeat = data._actor.data.items.find(
+
+    const hasWildMagicFeat =
+      data._actor.data.items.find(
         (a) => a.name === `Wild Magic Surge` && a.type === "feat"
       ) !== undefined;
     const is1stLevelOrHigher = data._item.labels.level.indexOf(" Level") > -1;
 
-    console.debug("is1stLevelOrHigher", is1stLevelOrHigher);
-    console.debug("hasWildMagicFeat", hasWildMagicFeat);
-    console.debug("Wild Magic Check?", hasWildMagicFeat && is1stLevelOrHigher);
-    console.debug("actor", data._actor.data.name);
-    console.debug("level", data._item.labels.level);
-  
     if (hasWildMagicFeat && is1stLevelOrHigher) {
       let text = `Wild Magic Check - Roll a D20`;
-  
+
       let chatData = {
         user: game.user.id,
         speaker: game.user,
@@ -48,6 +29,6 @@ Hooks.on("preRollItemBetterRolls", function (arg1, arg2, arg3) {
       ChatMessage.create(chatData, {});
     }
   } catch (e) {
-      console.error(`${MODULE_NAME}:`, e)
+    console.error(`${MODULE_NAME}`, e);
   }
 });
