0fe29a8356d0472eb0d46770b75f1088ca154402
John Nolan <me@johnnolan.dev>
2021-05-08 10:14:33 +0100

    Strip back with some debug code

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,3 @@
-
-import { wildMagicCheck } from './lib/lib.js';
-
 console.log(
     "Wild Magic Surge 5e"
   );
@@ -19,8 +16,33 @@ Hooks.on("ready", function () {
 
 Hooks.on("preRollItemBetterRolls", function (arg1, arg2, arg3) {
   console.log(
-    "Wild Magic Surge 5e Check"
+    "Wild Magic Surge 5e Check", arg1
   );
 
-  wildMagicCheck(arg1);
+  try {
+    //const arg = arguments[0].args[3][0];
+    const data = arg1[0].args[3][0];
+  
+    const hasWildMagicFeat = data._actor.data.items.find(
+        (a) => a.name === "Wild Magic Surge" && a.type === "feat"
+      ) !== undefined;
+    const is1stLevelOrHigher = data._item.labels.level.indexOf(" Level") > -1;
+
+    console.debug("is1stLevelOrHigher", is1stLevelOrHigher);
+    console.debug("hasWildMagicFeat", hasWildMagicFeat);
+    console.debug("Wild Magic Check?", hasWildMagicFeat && is1stLevelOrHigher);
+    console.debug("actor", arg._actor.data.name);
+    console.debug("level", arg._item.labels.level);
+  
+    if (hasWildMagicFeat && is1stLevelOrHigher) {
+      let text = `Wild Magic Check - Roll a D20`;
+  
+      let chatData = {
+        user: game.user.id,
+        speaker: game.user,
+        content: text,
+      };
+      ChatMessage.create(chatData, {});
+    }
+  } catch (e) {}
 });
