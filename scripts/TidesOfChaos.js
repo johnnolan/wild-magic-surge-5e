b6f89fb5bd6ccb9539e1ed915ed195a3cad8b27b
John Nolan <me@johnnolan.dev>
2021-05-25 09:09:51 +0100

    ToC actually recharges

--- scripts/TidesOfChaos.js
+++ scripts/TidesOfChaos.js
@@ -14,29 +14,15 @@ export async function TidesOfChaos(actor) {
     return;
   }
 
+  const resourceName = tidesItem.data.data.consume.target;
+
   let updates = [];
+  updates.push({
+    _id: tidesItem._id,
+    "data.uses.value": 1,
+    "data.recharge.charged": true,
+  });
 
-  // If betterRolls5e is enabled, set the following to ensure the spell recharges
-  if (tidesItem.flags["betterRolls5e"]) {
-    updates.push({
-      _id: tidesItem._id,
-      "data.uses.value": 1,
-      "data.recharge.charged": true,
-      "flags.betterRolls5e.quickCharges.value.use": false,
-      "flags.betterRolls5e.quickCharges.value.resource": false,
-      "flags.betterRolls5e.quickCharges.value.charge": true,
-      "flags.betterRolls5e.quickCharges.altValue.use": false,
-      "flags.betterRolls5e.quickCharges.altValue.resource": false,
-      "flags.betterRolls5e.quickCharges.altValue.charge": true,
-    });
-  } else {
-    updates.push({
-      _id: tidesItem._id,
-      "data.uses.value": 1,
-      "data.recharge.charged": true,
-    });
-  }
-
-  await actor.updateEmbeddedEntity("OwnedItem", updates);
+  await actor.update({ data: { [resourceName]: 1 }, items: updates });
   SendChat(game.settings.get(`${MODULE_ID}`, `${OPT_TOC_RECHARGE_MSG}`));
 }
