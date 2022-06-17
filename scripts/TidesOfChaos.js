b45951d45b54e8cc96cff943dfbe1114be67e2df
John Nolan <me@johnnolan.dev>
2022-06-17 07:36:10 +0100

    Start adding in function to check if TOC used. Needs refactor and removal of v8 code

--- scripts/TidesOfChaos.js
+++ scripts/TidesOfChaos.js
@@ -47,4 +47,40 @@ export default class TidesOfChaos {
     }
     await actor.update({ data: { [`${resourceName}`]: 1 } });
   }
+
+  async IsTidesOfChaosUsed(actor) {
+    const tidesItem = actor.data.items.find(
+      (a) =>
+        a.name === game.settings.get(`${MODULE_ID}`, `${OPT_TOC_NAME}`) &&
+        a.type === "feat"
+    );
+
+    if (tidesItem === undefined) {
+      // If not enabled or exists then return false indicating not used.
+      return false;
+    }
+
+    let isLolDataData = false;
+
+    let resourceName;
+    // TODO: Remove lol data for just v9?
+    if (tidesItem.data) {
+      if (tidesItem.data.consume) {
+        // Cannot tell if this is version 8 api change or a module moving the data
+        isLolDataData = true;
+        resourceName = `${tidesItem.data.consume.target}`;
+      } else {
+        resourceName = tidesItem.data.data.consume.target;
+      }
+    }
+
+    if (!resourceName) {
+      // If not enabled or exists then return true indicating not used.
+      return false;
+    }
+
+    if (tidesItem.data.uses.value === 1 && tidesItem.data.recharge.charged) {
+      return true;
+    }
+  }
 }
