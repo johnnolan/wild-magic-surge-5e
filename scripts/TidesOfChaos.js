2f2fb29a1d26d6cdb369e1689c1fbaec6384b1d8
John Nolan <me@johnnolan.dev>
2022-07-22 18:10:07 +0100

    Add Tides of Chaos Docs, Refactor and Tests (#124)
    * Refactor and add documentation to ToC
    
    * Add Tides of Chaos tests

--- scripts/TidesOfChaos.js
+++ scripts/TidesOfChaos.js
@@ -1,65 +1,84 @@
 import { MODULE_ID, OPT_ENABLE_TOC, OPT_TOC_NAME } from "./Settings.js";
 
-export default class TidesOfChaos {
+/**
+ * Controls the Tides of Chaos feat
+ * @class TidesOfChaos
+ * @example
+ * let tidesOfChaos = new TidesOfChaos();
+ */
+class TidesOfChaos {
   constructor() {}
 
+  /**
+   * Checks to see if Tides of Chaos has been used and if so recharge it
+   * @public
+   * @return {Promise<void>}
+   * @param {Actor} actor The Foundry Actor.
+   */
   async Check(actor) {
     if (!game.settings.get(`${MODULE_ID}`, `${OPT_ENABLE_TOC}`)) {
       return;
     }
+    const tidesItemData = await this.getTidesOfChaosResource(actor);
 
-    const tidesItem = actor.data.items.find(
-      (a) =>
-        a.name === game.settings.get(`${MODULE_ID}`, `${OPT_TOC_NAME}`) &&
-        a.type === "feat"
-    );
-
-    if (tidesItem === undefined) {
-      return;
-    }
-
-    let isLolDataData = false;
-
-    let resourceName = tidesItem?.data?.data?.consume?.target;
-
-    if (!resourceName) return false;
+    if (tidesItemData === undefined) return false;
 
     let updates = [];
     updates.push({
-      _id: tidesItem.id,
+      _id: tidesItemData.id,
       "data.uses.value": 1,
       "data.recharge.charged": true,
     });
 
-    if (isLolDataData) {
-      await actor.updateEmbeddedEntity("OwnedItem", updates);
-    } else {
-      await actor.update({ items: updates });
-    }
-    await actor.update({ data: { [`${resourceName}`]: 1 } });
+    await actor.update({ items: updates });
+    await actor.update({ data: { [`${tidesItemData.resourceName}`]: 1 } });
   }
 
+  /**
+   * Checks to see if Tides of Chaos has been used
+   * @public
+   * @return {Promise<boolean>}
+   * @param {Actor} actor The Foundry Actor.
+   */
   async IsTidesOfChaosUsed(actor) {
-    const tidesItem = actor.data.items.find(
-      (a) =>
-        a.name === game.settings.get(`${MODULE_ID}`, `${OPT_TOC_NAME}`) &&
-        a.type === "feat"
-    );
+    const tidesItemData = await this.getTidesOfChaosResource(actor);
 
-    if (tidesItem === undefined) {
-      // If not enabled or exists then return false indicating not used.
-      return false;
-    }
+    if (tidesItemData === undefined) return false;
 
-    if (!tidesItem?.data?.data?.consume?.target) {
-      // If not enabled or exists then return false indicating not used.
-      return false;
-    }
-
-    if (tidesItem.data.data.uses.value === 0) {
+    if (tidesItemData.usesLeft === 0) {
       return true;
     }
 
     return false;
   }
+
+  /**
+   * Fetches the actors Tides of Chaos data. If not available, returns undefined.
+   * @return {Promise<TidesItemData>}
+   * @param {Actor} actor The Foundry Actor.
+   */
+  async getTidesOfChaosResource(actor) {
+    const featName = game.settings.get(`${MODULE_ID}`, `${OPT_TOC_NAME}`);
+    const tidesItem = actor.data.items.find(
+      (a) => a.name === featName && a.type === "feat"
+    );
+
+    if (tidesItem === undefined) {
+      // If not enabled or exists then return false indicating not used.
+      return undefined;
+    }
+
+    if (!tidesItem?.data?.data?.consume?.target) {
+      // If not enabled or exists then return false indicating not used.
+      return undefined;
+    }
+
+    return {
+      usesLeft: tidesItem.data.data.uses.value,
+      id: tidesItem.id,
+      resourceName: tidesItem?.data?.data?.consume?.target,
+    };
+  }
 }
+
+export default TidesOfChaos;
---
+++ scripts/TidesOfChaos.test.js
@@ -0,0 +1,156 @@
+import TidesOfChaos from "./TidesOfChaos.js";
+import "../__mocks__/index.js";
+
+//jest.mock("./Chat.js");
+
+describe("TidesOfChaos", () => {
+  describe("If TidesOfChaos exists on the Actor", () => {
+    describe("And I want to check whether it is charged or not", () => {
+      describe("And it is not charged", () => {
+        let tidesOfChaos;
+        let actor;
+        beforeEach(() => {
+          global.game = {
+            settings: {
+              get: jest.fn().mockReturnValueOnce("Tides of Chaos"),
+            },
+          };
+          tidesOfChaos = new TidesOfChaos();
+          actor = {
+            data: {
+              items: {
+                find: jest.fn().mockReturnValueOnce({
+                  id: 1,
+                  name: "Tides of Chaos",
+                  type: "feat",
+                  data: {
+                    data: {
+                      uses: {
+                        value: 0,
+                      },
+                      consume: {
+                        target: "test",
+                      },
+                    },
+                  },
+                }),
+              },
+            },
+          };
+        });
+
+        it("should return true", async () => {
+          const result = await tidesOfChaos.IsTidesOfChaosUsed(actor);
+          expect(result).toBeTruthy();
+        });
+      });
+
+      describe("And it is charged", () => {
+        let tidesOfChaos;
+        let actor;
+        beforeEach(() => {
+          global.game = {
+            settings: {
+              get: jest.fn().mockReturnValueOnce("Tides of Chaos"),
+            },
+          };
+          tidesOfChaos = new TidesOfChaos();
+          actor = {
+            data: {
+              items: {
+                find: jest.fn().mockReturnValueOnce({
+                  id: 1,
+                  name: "Tides of Chaos",
+                  type: "feat",
+                  data: {
+                    data: {
+                      uses: {
+                        value: 1,
+                      },
+                      consume: {
+                        target: "test",
+                      },
+                    },
+                  },
+                }),
+              },
+            },
+          };
+        });
+
+        it("should return false", async () => {
+          const result = await tidesOfChaos.IsTidesOfChaosUsed(actor);
+          expect(result).toBeFalsy();
+        });
+      });
+    });
+
+    describe("And I want to check and reset if its not charged", () => {
+      describe("And it is not charged", () => {
+        let tidesOfChaos;
+        let actor;
+        beforeEach(() => {
+          global.game = {
+            settings: {
+              get: jest.fn().mockReturnValueOnce("Tides of Chaos"),
+            },
+          };
+          tidesOfChaos = new TidesOfChaos();
+          actor = {
+            update: jest.fn(),
+            data: {
+              items: {
+                find: jest.fn().mockReturnValueOnce({
+                  id: 1,
+                  name: "Tides of Chaos",
+                  type: "feat",
+                  data: {
+                    data: {
+                      uses: {
+                        value: 0,
+                      },
+                      consume: {
+                        target: "test",
+                      },
+                    },
+                  },
+                }),
+              },
+            },
+          };
+        });
+
+        it("should return true", async () => {
+          await tidesOfChaos.Check(actor);
+          expect(actor.update).toBeCalled();
+          expect(actor.update).toHaveBeenCalledTimes(2);
+        });
+      });
+    });
+  });
+
+  describe("If TidesOfChaos does not exist on the Actor", () => {
+    let tidesOfChaos;
+    let actor;
+    beforeEach(() => {
+      global.game = {
+        settings: {
+          get: jest.fn().mockReturnValueOnce("Tides of Chaos"),
+        },
+      };
+      tidesOfChaos = new TidesOfChaos();
+      actor = {
+        data: {
+          items: {
+            find: jest.fn().mockReturnValueOnce(undefined),
+          },
+        },
+      };
+    });
+
+    it("should return true", async () => {
+      const result = await tidesOfChaos.IsTidesOfChaosUsed(actor);
+      expect(result).toBeFalsy();
+    });
+  });
+});
