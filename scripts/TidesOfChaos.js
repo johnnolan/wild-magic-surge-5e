69910854deea66fcec95c08c0da6a75d99cd8372
John Nolan <me@johnnolan.dev>
2022-07-29 22:44:05 +0100

    Update tides of chaos to v10

--- scripts/TidesOfChaos.js
+++ scripts/TidesOfChaos.js
@@ -28,8 +28,8 @@ class TidesOfChaos {
     let updates = [];
     updates.push({
       _id: tidesItemData.id,
-      "data.uses.value": 1,
-      "data.recharge.charged": true,
+      "system.uses.value": 1,
+      "system.recharge.charged": true,
     });
 
     await actor.updateEmbeddedDocuments("Item", updates);
@@ -57,7 +57,7 @@ class TidesOfChaos {
    */
   async getTidesOfChaosResource(actor) {
     const featName = game.settings.get(`${MODULE_ID}`, `${OPT_TOC_NAME}`);
-    const tidesItem = actor.data.items.find(
+    const tidesItem = actor.items.find(
       (a) => a.name === featName && a.type === "feat"
     );
 
@@ -66,15 +66,15 @@ class TidesOfChaos {
       return undefined;
     }
 
-    if (!tidesItem?.data?.data?.consume?.target) {
+    if (!tidesItem?.system?.consume?.target) {
       // If not enabled or exists then return false indicating not used.
       return undefined;
     }
 
     return {
-      usesLeft: tidesItem.data.data.uses.value,
+      usesLeft: tidesItem.system.uses.value,
       id: tidesItem.id,
-      resourceName: tidesItem?.data?.data?.consume?.target,
+      resourceName: tidesItem?.system?.consume?.target,
     };
   }
 }
