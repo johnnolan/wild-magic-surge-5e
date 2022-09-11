c67911a37daba776d0c8f4c69b9e3f6a7458e3cd
John Nolan <me@johnnolan.dev>
2022-09-11 18:30:20 +0100

    Update example macro

--- scripts/macros/simple-check.js
+++ scripts/macros/simple-check.js
@@ -1,6 +1,16 @@
-console.log(this);
+// Actor data you can parse and use
 console.log(actor);
+
+// Token data you can parse and use
 console.log(token);
+
+// Current Die Descending value (d20, d12, d10, etc)
+var descendingDieType = actor.getFlag("wild-magic-surge-5e", "die_type").dieValue;
+
+// Current Incremental Check value (1-10)
+var incrementalCheckValue = actor.getFlag("wild-magic-surge-5e", "surge_increment_resource").value;
+
+// Gets whether the last check was a surge or not.
 var hasSurged = actor.getFlag("wild-magic-surge-5e", "hassurged");
 
 if (hasSurged) {
