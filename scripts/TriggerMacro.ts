78df320c66c9904b674895ab161b94d52061490c
John Nolan <me@johnnolan.dev>
2022-09-11 07:47:56 +0100

    Add TriggerMacro class

---
+++ scripts/TriggerMacro.ts
@@ -0,0 +1,38 @@
+import Logger from "./Logger";
+import { WMSCONST } from "./WMSCONST";
+
+/**
+ * Runs a macro
+ * @class TriggerMacro
+ */
+class TriggerMacro {
+  static async Run(): Promise<void> {
+    const macroName = game.settings.get(
+      `${WMSCONST.MODULE_ID}`,
+      `${WMSCONST.OPT_TRIGGERMACRO_NAME}`
+    );
+    if (
+      !game.settings.get(
+        `${WMSCONST.MODULE_ID}`,
+        `${WMSCONST.OPT_TRIGGERMACRO_ENABLE}`
+      ) ||
+      !macroName
+    ) {
+      return;
+    }
+
+    const macro = game.macros.find((f) => f.name === macroName);
+    if (!macro) {
+      Logger.error(
+        `Trigger Macro ${macroName} does not exist.`,
+        "TriggerMacro.Run",
+        macroName
+      );
+      return;
+    }
+
+    macro.execute();
+  }
+}
+
+export default TriggerMacro;
